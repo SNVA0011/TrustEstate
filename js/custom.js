e9abc00b03895b35e6c16f94c3f68898343af63a
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2024-03-12 18:35:28 +0530

    Homepage

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,34 @@
-var section = document.querySelector('.HappyCustomerAchv');
+
+
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start ? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function () {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+
+var sectionAchv = document.querySelector('.HappyCustomerAchv');
 var hasEntered = false;
 window.addEventListener('scroll', (e) => {
-    var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop + 82;
+    var shouldAnimate = (window.scrollY + window.innerHeight) >= sectionAchv.offsetTop + 82;
 
     if (shouldAnimate && !hasEntered) {
         hasEntered = true;
 
-        function animateValue(id, start, end, duration) {
-            if (start === end) return;
-            var range = end - start;
-            var current = start;
-            var increment = end > start ? 1 : -1;
-            var stepTime = Math.abs(Math.floor(duration / range));
-            var obj = document.getElementById(id);
-            var timer = setInterval(function () {
-                current += increment;
-                obj.innerHTML = current;
-                if (current == end) {
-                    clearInterval(timer);
-                }
-            }, stepTime);
-        }
-
         animateValue("hapcusnum", 0, 12, 2000);
         animateValue("offlocnum", 0, 6, 1500);
         animateValue("proptynum", 0, 20, 3000);
-        animateValue("agenetnum", 0, 85, 5000);
+        animateValue("agenetnum", 0, 85, 5000); 
     }
 
     //------------ headerSite ------------
@@ -43,5 +46,13 @@ window.addEventListener('scroll', (e) => {
     }
 
 });
+
+
+window.addEventListener('DOMContentLoaded',function () {
+    animateValue("ourhappyrate", 0, 10, 1500);
+    animateValue("exclagents", 0, 8, 1500); 
+});
+
+
 
 
